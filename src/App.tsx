2425fd4a1a0998285b91b484cc9a612dfa7840c2
Larsbuilds <larstischer@icloud.com>
2025-03-31 10:25:44 +0300

    feat: implement dark mode functionality with theme context and toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,26 @@ import { ReadingView } from '@/components/ReadingView/ReadingView';
 import { SearchView } from '@/components/SearchView/SearchView';
 import { BibleProvider } from '@/contexts/BibleContext';
 import { SearchProvider } from '@/contexts/SearchContext';
+import { ThemeProvider } from '@/contexts/ThemeContext';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <BibleProvider>
-        <SearchProvider>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<Navigate to="/reading" replace />} />
-              <Route path="reading" element={<ReadingView />} />
-              <Route path="search" element={<SearchView />} />
-              {/* Add more routes as we implement features */}
-              <Route path="*" element={<Navigate to="/reading" replace />} />
-            </Route>
-          </Routes>
-        </SearchProvider>
-      </BibleProvider>
+      <ThemeProvider>
+        <BibleProvider>
+          <SearchProvider>
+            <Routes>
+              <Route path="/" element={<MainLayout />}>
+                <Route index element={<Navigate to="/reading" replace />} />
+                <Route path="reading" element={<ReadingView />} />
+                <Route path="search" element={<SearchView />} />
+                {/* Add more routes as we implement features */}
+                <Route path="*" element={<Navigate to="/reading" replace />} />
+              </Route>
+            </Routes>
+          </SearchProvider>
+        </BibleProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
